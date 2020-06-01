bc3bbb8935c7cedabb8caf8a6724cb73738c704c
fhe-fr <16428049+fhe-fr@users.noreply.github.com>
2020-06-01 16:48:55 +0200

    Update serving_build_container_image.py

--- notebooks/serving_build_container_image.py
+++ notebooks/serving_build_container_image.py
@@ -25,9 +25,14 @@ print(" FHE resource_group = ",resource_group)
 subscription_id = dbutils.secrets.get(scope = "azureml", key = "subscription_id")
 print(" FHE subscription_id = ",subscription_id)
 
+ervice_principal_id = dbutils.secrets.get(scope = "azureml", key = "client_id")
 print(" FHE client_id = ",client_id)
+
+tenant_id = dbutils.secrets.get(scope = "azureml", key = "tenant_id")
 print(" FHE tenant_id = ",tenant_id)
+
 print(" ************************************* ")
+service_principal_password = dbutils.secrets.get(scope = "azureml", key = "client_secret")
 print(" FHE client_secret = ",client_secret)
 print(" ************************************* ")
 
